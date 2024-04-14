ab02b68113943d7f8132bd93728b94605d2dc8fe
alexrf32 <72366671+alexrf32@users.noreply.github.com>
2024-04-14 19:07:47 -0400

    feat: add GetPasswordDtoAsync

--- Src/Interfaces/IUserRepository.cs
+++ Src/Interfaces/IUserRepository.cs
@@ -1,8 +1,11 @@
+using courses_dotnet_api.Src.DTOs.Account;
+
 namespace courses_dotnet_api.Src.Interfaces;
 
 public interface IUserRepository
 {
     Task<bool> UserExistsByEmailAsync(string email);
     Task<bool> UserExistsByRutAsync(string rut);
+    Task<PasswordDto?> GetPasswordDtoAsync(string email);
     Task<bool> SaveChangesAsync();
 }
